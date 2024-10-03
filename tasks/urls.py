ed1c2ad6ae13dcfb878fb8203ca04909f2c09e2d
AXS05000 <axssxs00@gmail.com>
2024-10-02 23:34:06 -0300

    V - 02-10-2024 - 23:34

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import atualizar_tarefas
+from .views import atualizar_tarefas, Profile_TasksView
 
 urlpatterns = [
     path("atualizar-tarefas/", atualizar_tarefas, name="atualizar_tarefas"),
+    path("profile-tasks/", Profile_TasksView.as_view(), name="profile_tasks"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,8 @@
 from django.http import JsonResponse
 from .services import buscar_tarefas_pendentes
 from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+from .models import TarefaClickUp
 
 
 @login_required
@@ -8,3 +10,14 @@ def atualizar_tarefas(request):
     usuario = request.user  # Pega o usuário logado
     buscar_tarefas_pendentes(usuario)
     return JsonResponse({"status": "Tarefas atualizadas com sucesso!"})
+
+
+class Profile_TasksView(TemplateView):
+    template_name = "pages/profile-tasks.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        usuario = self.request.user
+        # Filtrando as tarefas do ClickUp pelo usuário logado
+        context["tarefas"] = TarefaClickUp.objects.filter(usuario=usuario)
+        return context
