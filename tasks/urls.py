d599f05cdc123aa678a4faea26b014f97ad35065
AXS05000 <axssxs00@gmail.com>
2024-10-02 23:50:55 -0300

    V - 02-10-2024 - 23:50

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
-from .views import atualizar_tarefas, Profile_TasksView
+from .views import (
+    atualizar_tarefas,
+    Profile_TasksView,
+    concluir_tarefas_clickup,
+    atualizar_tarefas_clickup,
+)
 
 urlpatterns = [
     path("atualizar-tarefas/", atualizar_tarefas, name="atualizar_tarefas"),
     path("profile-tasks/", Profile_TasksView.as_view(), name="profile_tasks"),
+    path(
+        "concluir-tarefas/", concluir_tarefas_clickup, name="concluir_tarefas_clickup"
+    ),
+    path(
+        "atualizar-tarefas-clickup/",
+        atualizar_tarefas_clickup,
+        name="atualizar_tarefas_clickup",
+    ),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -3,6 +3,9 @@ from .services import buscar_tarefas_pendentes
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 from .models import TarefaClickUp
+import requests
+from django.shortcuts import redirect
+from django.views.decorators.http import require_POST
 
 
 @login_required
@@ -10,6 +13,15 @@ def atualizar_tarefas(request):
     usuario = request.user  # Pega o usuário logado
     buscar_tarefas_pendentes(usuario)
     return JsonResponse({"status": "Tarefas atualizadas com sucesso!"})
+
+
+@login_required
+def atualizar_tarefas_clickup(request):
+    usuario = request.user
+    buscar_tarefas_pendentes(
+        usuario
+    )  # Função que atualiza as tarefas no banco de dados
+    return redirect("profile_tasks")  # Redireciona de volta para a página de perfil
 
 
 class Profile_TasksView(TemplateView):
@@ -23,3 +35,39 @@ class Profile_TasksView(TemplateView):
             data_inicial__isnull=True
         )
         return context
+
+
+@require_POST
+def concluir_tarefas_clickup(request):
+    usuario = request.user
+    tarefas_ids = request.POST.getlist(
+        "tarefas_concluidas"
+    )  # IDs das tarefas selecionadas
+
+    for tarefa_id in tarefas_ids:
+        # Concluir a tarefa no ClickUp
+        concluir_tarefa_clickup(tarefa_id, usuario.clickup_api_token)
+
+        # Excluir a tarefa do sistema
+        TarefaClickUp.objects.filter(tarefa_id=tarefa_id, usuario=usuario).delete()
+
+    return redirect("profile_tasks")
+
+
+def concluir_tarefa_clickup(tarefa_id, clickup_token):
+    url = f"https://api.clickup.com/api/v2/task/{tarefa_id}"
+
+    headers = {
+        "Authorization": clickup_token,
+    }
+
+    data = {
+        "status": "complete",  # Status de conclusão
+    }
+
+    response = requests.put(url, headers=headers, json=data)
+
+    if response.status_code != 200:
+        print(
+            f"Erro ao concluir tarefa {tarefa_id}: {response.status_code} - {response.text}"
+        )
