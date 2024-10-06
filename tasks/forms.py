1fe83c8e24f0a5cb80bd31156963ee10459f051b
AXS05000 <axssxs00@gmail.com>
2024-10-05 23:36:02 -0300

    V - 05-10-2024 - 23:35

--- tasks/forms.py
+++
@@ -1,9 +0,0 @@
-# forms.py
-from django import forms
-
-
-class UdemyAuthForm(forms.Form):
-    client_id = forms.CharField(label="ID do Cliente", max_length=255)
-    client_secret = forms.CharField(
-        label="Segredo do Cliente", max_length=255, widget=forms.PasswordInput
-    )
